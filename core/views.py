3d5296ea854346b6756a794990dad9a49188f131
Luis Molina <luis_molina@outlook.com>
2018-01-12 04:41:26 -0700

    Getting access_token.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
 @login_required
 def home(request):
-    return render(request, 'core/home.html')
+    user = request.user.social_auth.get(provider='github')
+    access_token = user.extra_data['access_token']
+    context = {'access_token': access_token}
+    return render(request, 'core/home.html', context=context)
--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Americas/Denver'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
